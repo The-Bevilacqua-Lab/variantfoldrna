4c015a5563e4e410c40540631e6fbe618fe2b253
Kobie-Kirven <kjkirven@presby.edu>
2023-01-13 14:57:38 -0500

    Update layout

--- workflow/scripts/plot_output.py
+++ workflow/scripts/plot_output.py
@@ -57,10 +57,10 @@ import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.graph_objects as go
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.SPACELAB])
+app = Dash(__name__, external_stylesheets=[dbc.themes.CERULEAN])
 
 heading = html.H4(
-    html.Img(src="/Users/lab/Desktop/snakemake/SPARCS/static/SPARCS.png", style={"height": "50px"}),
+    html.Img(src="/Users/lab/Desktop/snakemake/SPARCS/static/SPARCS.png"),
     "SPARCS Ouput Analysis", className="bg-primary text-white p-2"
 )
 
@@ -69,6 +69,23 @@ df = df1.iloc[:,[0,1,2,3,5,6,8,9]]
 df.columns = ["chrom", "pos", "ref", "alt", "gene", "match", "strand", "score"]
 ops = [{'label': 'All', 'value': 'All'}]
 ops += [{'label': i, 'value': i} for i in df['gene'].unique()]
+
+# the style arguments for the sidebar. We use position:fixed and a fixed width
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
 
 def get_data(df, drop_value):
     if drop_value == "All":
@@ -82,27 +99,32 @@ gene = html.Div(
         options = ops,
         value = 'gene',),
     ],
-    className="mt-2",
+    className="mt-2", style=SIDEBAR_STYLE
 )
 
-control_panel = dbc.Card(
-    dbc.CardBody(
-        [gene],
-        className="bg-light",
-    )
-)
+# control_panel = dbc.Card(
+#     dbc.CardBody(
+#         [gene],
+#         className="bg-light",
+#     ), style=SIDEBAR_STYLE,
+# )
 
-graph = dbc.Card(
-    [html.Div(id="error_msg", className="text-danger"), dcc.Graph(id="bar_plot")]
-)
+# graph = dbc.Card(
+#     [html.Div(id="gene_s", className="text-danger"),dcc.Graph(id="gene_struct_plot"), dcc.Graph(id="bar_plot")]
+# )
 
-gene_struct = dbc.Card(
-    [html.Div(id="error_msg", className="text-danger"), dcc.Graph(id="gene_struct_plot")]
-)
+# gene_struct = dbc.Card(
+#     [html.Div(id="error_mss", className="text-danger")]
+# )
 
-app.layout = html.Div(
-    [heading, dbc.Row([dbc.Col(control_panel, md=4), dbc.Col(graph, md=8)]), dbc.Row([dbc.Col(gene_struct, md=8)])]
-)
+content = html.Div(dbc.Row([dcc.Graph(id = 'gene_struct_plot'), dcc.Graph(id = 'bar_plot')]), style=CONTENT_STYLE)
+
+
+# the styles for the main content position it to the right of the sidebar and
+# add some padding.
+
+# Layout for the entire page
+app.layout = html.Div([heading, gene, content])
 
 @app.callback(Output(component_id='bar_plot', component_property= 'figure'),
               [Input(component_id='dropdown', component_property= 'value')])
@@ -120,7 +142,47 @@ def graph_update(dropdown_value):
 @app.callback(Output(component_id='gene_struct_plot', component_property= 'figure'),
               [Input(component_id='dropdown', component_property= 'value')])
 def graph_update(dropdown_value):
-    fig = go.Figure(go.Scatter(x=[3,3,5,5,3], y=[0.5,1.5,1.5,0.5,0.5], fill="toself"))
+    fig = go.Figure()
+    fig.add_shape(type="rect",
+    x0=0, y0=1, x1=2, y1=1,
+    line=dict(
+        color="RoyalBlue",
+        width=2,
+    ),
+    fillcolor="LightSkyBlue",
+    
+)
+
+    fig.add_shape(type="rect",
+    x0=2, y0=2, x1=6, y1=2,
+    line=dict(
+        color="green",
+        width=2,
+    ),
+    fillcolor="LightSkyBlue",
+    
+)
+    # Update axes properties
+    fig.update_xaxes(
+        showticklabels=False,
+        showgrid=False,
+        zeroline=False,
+    )
+
+    fig.update_yaxes(
+        showticklabels=False,
+        showgrid=False,
+        zeroline=False,
+    )
+
+    fig.update_shapes(opacity=0.3, xref="x", yref="y")
+
+    fig.update_layout(
+        margin=dict(l=20, r=20, b=100),
+        height=600, width=800,
+        plot_bgcolor="white"
+    )
+    fig.update_traces(mode="markers+lines")
     return fig
 
 if __name__ == "__main__":
