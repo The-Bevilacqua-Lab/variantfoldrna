9a24ac48b45bc34ed7efa8261ec04a184fd0c36e
Kobie-Kirven <kjkirven@presby.edu>
2023-02-21 09:32:22 -0500

    Add test for chunking with gzipped files

--- sparcs/sparcs_workflow/scripts/chunk_vcf.py
+++ sparcs/sparcs_workflow/scripts/chunk_vcf.py
@@ -28,6 +28,26 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
+# -- Functions --   #
+def get_vcf_len(file_name, gzip_file=False):
+    """
+    Get the number of lines in the VCF file
+    """
+    total = 0
+    if gzip_file:
+        print(file_name)
+        fn = gzip.open(file_name, 'rb')
+    else:
+        fn = open(file_name)
+    for line in fn:
+        if line.startswith("#".encode()):
+            continue
+        else:
+            total += 1
+    return total
+
+# -- Main --   #
+
 # Check to see if we are dealing with a gzipped VCF file or not
 if args.input.endswith(".gz"):
     vcf_gz = True
@@ -42,24 +62,6 @@ if not os.path.exists(f"{args.dir}/vcf_chunks/"):
         prRed("Error: Can not create directory to hold VCF chunks")
         sys.exit(1)
 
-def get_vcf_len(file_name, gzip_file=False):
-    """
-    Get the number of lines in the VCF file
-    """
-    total = 0
-
-    if gzip_file:
-        print(file_name)
-        fn = gzip.open(file_name, 'rb')
-    else:
-        fn = open(file_name)
-    for line in fn:
-        if line.startswith("#".encode()):
-            continue
-        else:
-            total += 1
-    return total
-
 # Get the number of lines in the VCF file
 vcf_len = get_vcf_len(args.input, gzip_file=vcf_gz)
 
@@ -70,32 +72,40 @@ else:
     # Get the length of the chunks
     chunk_len = vcf_len // (int(args.chunk) - 1)
 
-# Split the VCF file up into tiny files
-
 # If the VCF file is gzipped, we need to open it differently
 if vcf_gz:
     in_file = gzip.open(args.input, "rb")
 else:
     in_file = open(args.input, "r")
 
+# Read in the first line of the VCF file which is the column header
 header_one = in_file.readline()
 for i in range(1, int(args.chunk)):
     fn = gzip.open(f"{args.dir}/vcf_chunks/vcf_no_header_{i}.vcf.gz", "wb")
+
+    # Write the header to the file
     with gzip.open(args.header, "rb") as header:
         for line in header:
             fn.write(line)
+
+    # Add the column header to the file
     fn.write(header_one)
+
+    # Write the VCF lines to the file
     for g in range(chunk_len):
         fn.write(in_file.readline())
     fn.close()
 
 # Write what's left to the last file
 fn = gzip.open(f"{args.dir}/vcf_chunks/vcf_no_header_{int(args.chunk)}.vcf.gz", "wb")
+
+# Write the header to the file
 with gzip.open(args.header, "rb") as header:
     for line in header:
         fn.write(line)
 fn.write(header_one)
 
+# Write the VCF lines to the file
 for line in in_file:
     fn.write(line)
 fn.close()
