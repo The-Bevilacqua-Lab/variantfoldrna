6e71c7fca8ea8488a03f2eff52eca725b9214ad1
Kobie-Kirven <kjkirven@presby.edu>
2023-07-13 20:32:22 -0400

    Add bottom n% tails

--- sparcs/sparcs_workflow/rules/get_top_percent.smk
+++ sparcs/sparcs_workflow/rules/get_top_percent.smk
@@ -8,12 +8,13 @@
 # Path to the config file
 configfile: srcdir("../config.yaml")
 
-rule get_top_n_percent:
+rule get_top_bottom_n_percent:
     # Return the top n-percent of SNPs with the highest riboSNitch score
     input:
         f"{config['working_directory']}/{config['out_name']}/results/ribosnitch_predictions/combined_ribosnitch_prediction_{{temp_deg}}.txt",
     output:
-        f"{config['working_directory']}/{config['out_name']}/results/ribosnitch_predictions/top_{config['top_n_percent']}_percent_{{temp_deg}}.txt"
+        top = f"{config['working_directory']}/{config['out_name']}/results/ribosnitch_predictions/top_{config['top_n_percent']}_percent_{{temp_deg}}.txt",
+        bottom = f"{config['working_directory']}/{config['out_name']}/results/ribosnitch_predictions/bottom_{config['top_n_percent']}_percent_{{temp_deg}}.txt",
     shell:
-        f"python3 workflow/scripts/get_top_n_percent.py --input {{input}} --output {{output}} --top-n-percent {config['top_n_percent']}"
+        f"python3 workflow/scripts/get_n_percent_tails.py --input {{input}} --top-output {{output.top}}  --bottom-output {{output.bottom}} --n-percent {config['top_n_percent']}"
 
--- sparcs/sparcs_workflow/scripts/get_n_percent_tails.py
+++ sparcs/sparcs_workflow/scripts/get_n_percent_tails.py
@@ -32,6 +32,28 @@ def get_top_n_percent(riboSNitch_predictions, top_n_percent):
     # Return the top n_percent of riboSNitches
     return top_n_percent_riboSNitches
 
+def get_bottom_n_percent(riboSNitch_predictions, top_n_percent):
+    """
+    Get the bottom n_percent of riboSNitches from the riboSNitch predictions
+    """
+    # Read in the riboSNitch predictions
+    riboSNitch_predictions_df = pd.read_csv(riboSNitch_predictions, sep="\t")
+
+    # Sort the dataframe by the score column
+    riboSNitch_predictions_df = riboSNitch_predictions_df.sort_values(by=['Score'])
+
+    # Get the number of rows in the dataframe
+    num_rows = riboSNitch_predictions_df.shape[0]
+
+    # Get the number of rows to keep
+    num_rows_to_keep = int(num_rows * top_n_percent)
+
+    # Get the bottom n_percent of riboSNitches
+    bottom_n_percent_riboSNitches = riboSNitch_predictions_df.tail(num_rows_to_keep)
+
+    # Return the bottom n_percent of riboSNitches
+    return bottom_n_percent_riboSNitches
+
 # ----- Main ------#
 if __name__ == "__main__":
 
@@ -46,21 +68,36 @@ if __name__ == "__main__":
         required=True,
     )
     parser.add_argument(
-        "--output",
-        dest="output",
+        "--top-output",
+        dest="top_output",
         help="Path to the output file",
         required=True,
     )
+
     parser.add_argument(
-        "--top-n-percent",
+        "--bottom-output",
+        dest="bottom_output",
+        help="Path to the output file",
+        required=True,
+    )
+
+    parser.add_argument(
+        "--n-percent",
         dest="top_n_percent",
         help="Top n percent of riboSNitches to keep",
         required=True,
     )
+
     args = parser.parse_args()
 
     # Get the top n_percent of riboSNitches
     top_n_percent_riboSNitches = get_top_n_percent(args.riboSNitch_predictions, float(args.top_n_percent))
 
     # Write the top n_percent of riboSNitches to a file
-    top_n_percent_riboSNitches.to_csv(args.output, sep="\t", index=False)
+    top_n_percent_riboSNitches.to_csv(args.top_output, sep="\t", index=False)
+
+    # Get the bottom n_percent of riboSNitches
+    bottom_n_percent_riboSNitches = get_bottom_n_percent(args.riboSNitch_predictions, float(args.top_n_percent))
+
+    # Write the bottom n_percent of riboSNitches to a file
+    bottom_n_percent_riboSNitches.to_csv(args.bottom_output, sep="\t", index=False)
