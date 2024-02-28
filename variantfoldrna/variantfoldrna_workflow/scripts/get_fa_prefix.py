5fffbcf43f9e553ca2ed70750256767a6fb414de
Kobie-Kirven <kjkirven@presby.edu>
2024-02-28 13:33:19 -0500

    Add get_fasta_prefix.py

---
+++ variantfoldrna/variantfoldrna_workflow/scripts/get_fa_prefix.py
@@ -0,0 +1,38 @@
+# Extract the prefix from the fasta file of extracted sequences
+
+# Imports 
+from Bio import SeqIO
+import argparse
+
+# Function to extract the prefix from the fasta file
+def get_fa_prefix(fasta_file):
+    # Open the fasta file
+    with open(fasta_file, "r") as handle:
+        # Get the first sequence
+        record = next(SeqIO.parse(handle, "fasta"))
+        # Get the prefix
+        prefix = record.id.split(":")[0]
+
+        # if len(record.id.split(":")) == 1:
+        #     prefix = ""
+            
+    # Return the prefix
+    return prefix
+
+# Main function
+def main():
+    # Get the fasta file from the user
+    parser = argparse.ArgumentParser(description="Extract the prefix from the fasta file of extracted sequences")
+    parser.add_argument("fasta_file", help="Fasta file of extracted sequences")
+    parser.add_argument("output_file", help="Output file to write the prefix to")
+    args = parser.parse_args()
+    # Get the prefix
+    prefix = get_fa_prefix(args.fasta_file)
+    # Print the prefix
+    with open(args.output_file, "w") as handle:
+        handle.write(prefix)
+
+# run main 
+
+if __name__ == "__main__":
+    main()
